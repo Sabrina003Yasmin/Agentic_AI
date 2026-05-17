c6de192cc0f755577b92c7f372bea62f4e3c6217
Cynthia Sabrin <sabrinayasmin003@gmail.com>
2026-05-17 20:27:38 +0600

    Add files via upload

---
+++ Assignment_01.py
@@ -0,0 +1,30 @@
+# Part 1
+
+# name variable
+name = "Sabrina"
+print(f"My name is {name}")
+
+# age variable
+age = 21
+print("My age is", age)
+
+# favorite color variable
+favorite_color = "Blue"
+print("My favorite color is", favorite_color)
+
+
+# Part 2
+
+# assigning values
+a = 5
+b = 10
+c = 15
+
+# printing all variables together
+print("Values are:", a, b, c)
+
+# finding total
+total = a + b + c
+
+# printing total
+print("Total =", total)
